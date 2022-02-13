f218c9ddc5019cb11a143020401ca5750db62bda
mstfrs <mstfrs68@gmail.com>
2022-02-14 00:16:17 +0300

    updated

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from multiprocessing.sharedctypes import Value
 from statistics import mode
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
