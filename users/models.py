56909c85d1aceb951f6672a35c8f589d5ff502c5
mstfrs <mstfrs68@gmail.com>
2022-02-14 01:14:13 +0300

    migration

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ from PIL import Image
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_images')
+    image = models.ImageField( upload_to='profile_images')
     
 
     def __str__(self):
